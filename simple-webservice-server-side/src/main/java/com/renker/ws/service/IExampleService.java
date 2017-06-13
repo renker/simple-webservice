d04ffc659c9afec45c10e025352a5b6e2fa32114
renker <caishenchen@163.com>
2017-06-13 14:08:30 +0800

    提交

--- simple-webservice-server-side/src/main/java/com/renker/ws/service/IExampleService.java
+++ simple-webservice-server-side/src/main/java/com/renker/ws/service/IExampleService.java
@@ -2,7 +2,7 @@ package com.renker.ws.service;
 
 import javax.jws.WebService;
 
-@WebService
+@WebService(name="exampleService",targetNamespace="http://www.renker.com/ws")
 public interface IExampleService {
 	public String Hellow(String msg);
 }
--- simple-webservice-server-side/src/main/java/com/renker/ws/service/impl/ExampleServiceImpl.java
+++ simple-webservice-server-side/src/main/java/com/renker/ws/service/impl/ExampleServiceImpl.java
@@ -1,7 +1,12 @@
 package com.renker.ws.service.impl;
 
+import javax.jws.WebService;
+
+import org.springframework.stereotype.Component;
+
 import com.renker.ws.service.IExampleService;
 
+@Component("exampleService")
 public class ExampleServiceImpl implements IExampleService{
 
 	public String Hellow(String msg) {
