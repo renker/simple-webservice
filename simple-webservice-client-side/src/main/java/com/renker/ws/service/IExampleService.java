6e6ba7259c686305d6167728d00b6a59f54dfbe0
renker <caishenchen@163.com>
2017-06-20 09:45:53 +0800

    ct

--- simple-webservice-client-side/src/main/java/com/renker/ws/service/IExampleService.java
+++ simple-webservice-client-side/src/main/java/com/renker/ws/service/IExampleService.java
@@ -1,7 +1,5 @@
 package com.renker.ws.service;
 
-import javax.jws.WebService;
-
 public interface IExampleService {
 	public String Hellow(String msg);
 }
--- simple-webservice-server-side/src/main/java/com/renker/ws/service/impl/ExampleServiceImpl.java
+++ simple-webservice-server-side/src/main/java/com/renker/ws/service/impl/ExampleServiceImpl.java
@@ -7,10 +7,11 @@ import org.springframework.stereotype.Component;
 import com.renker.ws.service.IExampleService;
 
 @Component("exampleService")
+@WebService(name="exampleService",targetNamespace="http://www.renker.com/ws")
 public class ExampleServiceImpl implements IExampleService{
 
 	public String Hellow(String msg) {
-		return "Server response :"+ msg == null? "NULL":msg;
+		return "Server response :"+ (msg == null? "NULL":msg);
 	}
 
 }
